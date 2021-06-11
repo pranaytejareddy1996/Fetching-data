6aac59eee3286974f425f22e62f6440ed69f8362
pranaytejareddy1996 <60606116+pranaytejareddy1996@users.noreply.github.com>
2021-06-11 11:05:05 +0530

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -5,8 +5,9 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>this is a navbar</h1>
             <div className="links">
-                <Link to ="/">home</Link>
+                <Link to ="/" style={{color:'white', backgroundColor:'#f1356d', borderRadius:'8px'}} >home</Link>
                 <Link to="/create" style={{color:'white', backgroundColor:'#f1356d', borderRadius:'8px'}}>new blog</Link>
+                <Link to='/photos' style={{color:'white', backgroundColor:'#f1356d', borderRadius:'8px'}}> photos </Link>
             </div>
         </nav>
      )
